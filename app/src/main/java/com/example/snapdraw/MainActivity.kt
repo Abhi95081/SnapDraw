5c31d8ce8ac9bebc468ce8479245f3dfa2005c73
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-09-11 14:47:35 +0530

    main.kt Commit

--- app/src/main/java/com/example/snapdraw/MainActivity.kt
+++ app/src/main/java/com/example/snapdraw/MainActivity.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.snapdraw.ui.theme.SnapDrawTheme
@@ -57,5 +58,12 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
+@RequiresApi(Build.VERSION_CODES.Q)
+@Preview(showBackground = true)
+        @Composable
+    fun GreetingPreview() {
+        SnapDrawTheme {
+            SnapDrawApp()
+        }
+    }
 
