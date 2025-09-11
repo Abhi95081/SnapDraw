9d552ea1b26821565bbbfcecfd8924b9726777ae
Abhishek Roushan <147068739+Abhi95081@users.noreply.github.com>
2025-09-11 14:48:00 +0530

    SnapDrawScreen.kt Commit

--- app/src/main/java/com/example/snapdraw/screens/SnapDrawScreen.kt
+++ app/src/main/java/com/example/snapdraw/screens/SnapDrawScreen.kt
@@ -47,6 +47,7 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -137,7 +138,7 @@ fun SnapDrawScreen() {
 
     // grid/snapping/hud
     var showGrid by remember { mutableStateOf(true) }
-    var gridSpacingPx by remember { mutableStateOf(40f) }
+    var gridSpacingPx by remember { mutableFloatStateOf(40f) }
     var snapEnabled by remember { mutableStateOf(true) }
     val snackbarHostState = remember { SnackbarHostState() }
 
